6c08fc845db7c443c306a18661d3bb172c47954a
Mohammad Pourbehzad <pspipm27@gmail.com>
2025-03-29 01:04:58 +0330

    implement constructor and styles

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 type FlashType = 'success' | 'error' | 'warning' | 'info';
 
 interface Config {
-    offset: number;
-    gap: number;
     icons: boolean;
     animations: boolean;
     direction: 'ltr' | 'rtl';
-    xAlign: 'center' | 'right' | 'left';
+    xAlign: 'right' | 'left';
     yAlign: 'top' | 'bottom';
     borderRadius: number;
-    fontFamily: string;
+    fontFamily?: string;
     types: Record<FlashType, FlashTypeConfig>;
 }
 
@@ -19,24 +17,16 @@ interface FlashTypeConfig {
     borderColor: string;
 }
 
-interface ItemConfig {
-    closable: boolean;
-    closeTimeout: number;
-}
-
 export default class Flash {
-    private static list = [];
+    private static list: Flash[] = [];
 
     static config: Config = {
-        offset: 20,
-        gap: 10,
         icons: true,
         animations: true,
         direction: 'ltr',
         xAlign: 'left',
         yAlign: 'top',
         borderRadius: 16,
-        fontFamily: 'Arial',
         types: {
             success: {
                 color: '#4CAF50',
@@ -63,11 +53,61 @@ export default class Flash {
 
     item = document.createElement("div");
 
-    constructor(
-        text: string,
-        type: FlashType,
-        {closable = true, closeTimeout = 5000}: ItemConfig
-    ) {
+    constructor(text: string, type: FlashType, {closable = true, closeTimeout = 5000}) {
+        Flash.list.push(this);
 
+        /* Add classes */
+        this.item.classList.add(`fl-item`);
+        this.item.classList.add(`fl-item-${Flash.config.xAlign}`);
+
+        /* Add styles */
+        this.item.style.direction = Flash.config.direction;
+        this.item.style.borderRadius = Flash.config.borderRadius + "px";
+        this.item.style.color = Flash.config.types[type].color;
+        this.item.style.backgroundColor = Flash.config.types[type].backgroundColor;
+        this.item.style.outlineColor = Flash.config.types[type].borderColor;
+
+        /* Apply animations */
+        this.item.dataset.animation = Flash.config.animations ? "1" : "0";
+
+        /* Apply fontFamily */
+        if (Flash.config.fontFamily)
+            this.item.style.fontFamily = Flash.config.fontFamily;
+
+        /* Add icon */
+        if (Flash.config.icons) {
+            const icon = document.createElement("img");
+            icon.classList.add("fl-icon");
+            icon.src = `icons/${type}.svg`;
+            icon.alt = type;
+            this.item.appendChild(icon);
+        }
+
+        /* Add content */
+        const content = document.createElement("div");
+        content.classList.add("fl-text");
+        content.innerHTML = text;
+        this.item.appendChild(content);
+
+        /* Close listener */
+        if (closable)
+            this.item.addEventListener("click", this.close);
+
+        /* Apply close timeout */
+        if (closeTimeout > 0) {
+            setTimeout(this.close, closeTimeout);
+        }
+
+        document.body.appendChild(this.item);
+    }
+
+    close(): void {
+        this.item.style.opacity = "0";
+        setTimeout(() => {
+            this.item.remove();
+        }, 500);
+
+        const index = Flash.list.indexOf(this);
+        Flash.list.splice(index, 1);
     }
 }
